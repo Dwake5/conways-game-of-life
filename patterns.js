15266d63baf5a2dd8be0f83d50d2600735711b42
Danny Wakeling <wakeling101@hotmail.com>
2020-04-22 13:06:26 +0100

    presets added and working

---
+++ patterns.js
@@ -0,0 +1,14 @@
+// import life.js from 
+
+// const populateCells = arr => {
+//     arr.forEach(element => {
+//         tiles[element[0]][element[1]].alive = true
+//     });
+// }
+
+// const stillLifes = () => {
+//     console.log('got here')
+//     freshGrid()
+//     let alive = [[1,1],[1,2],[2,1],[2,2]]
+//     populateCells(alive)
+// }
